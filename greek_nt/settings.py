02f6e45a87e0181faec39c12fde8e5c510a50157
Mike Brinker <themikejr@gmail.com>
2025-01-17 10:20:59 -0500

    Try fly.io volumes with SQLite.

--- greek_nt/settings.py
+++ greek_nt/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = "greek_nt.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "/data/db.sqlite3"
+        if os.getenv("ENVIRONMENT") == "production"
+        else BASE_DIR / "db.sqlite3",
     }
 }
 
